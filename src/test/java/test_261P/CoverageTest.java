b2084cba28db74110e01e18e0d3583c8aef7609e
68994510+pkakria@users.noreply.github.com <68994510+pkakria@users.noreply.github.com>
2021-02-13 01:28:48 -0800

    added test cases in CoverageTest.java

--- src/test/java/test_261P/CoverageTest.java
+++ src/test/java/test_261P/CoverageTest.java
@@ -1,8 +1,16 @@
 package test_261P;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.CompassPlot;
+import org.jfree.chart.plot.PiePlot;
+import org.jfree.data.general.DefaultPieDataset;
 import org.jfree.data.general.DefaultValueDataset;
+import org.jfree.data.general.PieDataset;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -60,4 +68,30 @@ public class CoverageTest {
        }
         assertTrue(pass);
     }
+    @Test
+    public void testCoverageChartFrame(){
+        DefaultPieDataset<String> data = new DefaultPieDataset<>();
+        data.setValue("Apple", 60);
+        data.setValue("Qualcomm", 45);
+        data.setValue("Amazon", 35);
+        try {
+            ChartFrame cf = new ChartFrame("My Title", ChartFactory.createPieChart("myChart", data));
+            Assertions.assertNotNull(cf);
+        }catch(Exception e){
+            Assertions.fail("should not throw exception");
+        }
+        try {
+            ChartFrame cf = new ChartFrame("My Title", ChartFactory.createPieChart("myChart", data), true);
+            Assertions.assertNotNull(cf);
+            JFreeChart chartObj = cf.getChartPanel().getChart();
+            Assertions.assertNotNull(chartObj);
+            PiePlot<String> plot = ((PiePlot<String>)chartObj.getPlot());
+            Assertions.assertEquals(plot.getDataset(), data);
+            Assertions.assertEquals(chartObj.getTitle().getText(), "myChart");
+        }catch(Exception e){
+            Assertions.fail("should not throw exception");
+        }
+
+    }
+
 }
