2e65e9e32e2ba922edd694ddee22eb502c1af7c8
68994510+pkakria@users.noreply.github.com <68994510+pkakria@users.noreply.github.com>
2021-02-28 03:07:59 -0800

    added tests

--- src/test/java/test_261P/MockingTest.java
+++ src/test/java/test_261P/MockingTest.java
@@ -10,6 +10,8 @@ import org.junit.Test;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -20,6 +22,7 @@ public class MockingTest {
     private ChartFactory chartFactory;
     private ChartFrame chartFrame;
     private Plot plot;
+    private ChartFrame frame;
 
     @Before
     public void setup() {
@@ -56,15 +59,59 @@ public class MockingTest {
     }
 
     @Test
-    public void testJFreeChartPlotInteraction(){
+    public void testSaveChartDataSetInteraction(){
         DefaultPieDataset<String> mockDataSet = mock(DefaultPieDataset.class);
         String [] keys = {"first", "second", "third"};
         int [] values = {10, 20, 30};
         when(mockDataSet.getKeys()).thenReturn(Arrays.asList(keys));
-        when(mockDataSet.getValue(0)).thenReturn(values[0]);
+        for (int i=0; i<values.length; i++){
+            when(mockDataSet.getValue(i)).thenReturn(values[i]);
+        }
+        for (int i=0; i<values.length; i++){
+            when(mockDataSet.getKey(i)).thenReturn(keys[i]);
+        }
+        String str;
+        when(mockDataSet.getItemCount()).thenReturn(keys.length);
+        when(mockDataSet.getValue("first")).thenReturn(10);
+        when(mockDataSet.getValue("second")).thenReturn(20);
+        when(mockDataSet.getValue("third")).thenReturn(30);
 
         JFreeChart pieChart = ChartFactory.createPieChart("Pie Chart", mockDataSet);
         ((PiePlot)(pieChart.getPlot())).getDataset().getKey(0);
+        // safe here. we know how many times mockDataSet() methods are called
+        verify(mockDataSet, times(1)).getKey(0);
+        verify(mockDataSet, never()).getKey(1);
+        verify(mockDataSet, never()).getKey(2);
+        //frame = new ChartFrame("My Chart", pieChart);
+        try {
+            ChartUtils.saveChartAsPNG(new File("../test3.png"),pieChart, 900, 900);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //
+         //getValue gets called for an item index
+        verify(mockDataSet, atMost(2)).getKey(0);
+        verify(mockDataSet, atMost(1)).getKey(1);
+        verify(mockDataSet, atMost(1)).getKey(1);
+
+        //verify that mockDataSet calls getValue(key) at least 3times for 3keys.
+        // Don't know how many times inner code is calling mockDataSet methods. Numbers are as high as12 to 36. So put atleast(1)
+        verify(mockDataSet, atLeast(3)).getValue(anyString());
+        verify(mockDataSet, atLeast(1)).getKeys();
+
 // verify data set was changed once
+    }
+
+    public static void main(String[] args) {
+        //example code to see a  chartFrame. Maybe can use in mocking in a test function.
+        MockingTest M = new MockingTest();
+        DefaultPieDataset<String> dataset = new DefaultPieDataset<>();
+        dataset.setValue("one", 1);
+        dataset.setValue("two", 2);
+        JFreeChart pieChart = ChartFactory.createPieChart("Pie Chart", dataset);
+
+        M.frame = new ChartFrame("My Chart", pieChart);
+        M.frame.pack();
+        M.frame.setVisible(true);
     }
 }
